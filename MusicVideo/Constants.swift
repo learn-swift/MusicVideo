cf4da3c69f0a8f2bce5724e35bcff5435353a8be
Duc Nguyen <nnduc@smartdev.vn>
2016-02-17 22:24:00 +0700

    finished part 4

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Duc Nguyen on 2/17/16.
+//  Copyright © 2016 ND NINJA. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
