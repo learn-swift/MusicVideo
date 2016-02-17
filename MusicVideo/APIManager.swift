7d60af59d27aaece2070773cd2dc38dadaed1933
Duc Nguyen <nnduc@smartdev.vn>
2016-02-17 23:12:45 +0700

    finished part 6

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class APIManager {
-	func loadData(urlString: String, completion : (result: String) -> Void) {
+	func loadData(urlString: String, completion : [Videos] -> Void) {
 		let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
 		let session = NSURLSession(configuration: config)
 		// let session = NSURLSession.sharedSession()
@@ -18,24 +18,33 @@ class APIManager {
 		let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
 			if error != nil {
 				dispatch_async(dispatch_get_main_queue()) {
-					completion(result: (error!.localizedDescription))
+					completion([])
 				}
 			} else {
 				do {
-					if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary {
-						print(json)
-						
-						let priority = DISPATCH_QUEUE_PRIORITY_HIGH
-						
-						dispatch_async(dispatch_get_global_queue(priority, 0)) {
-							dispatch_async(dispatch_get_main_queue()) {
-								completion(result: "JSONSerialization Successful")
+					if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary,
+						feed = json["feed"] as? JSONDictionary,
+						entries = feed["entry"] as? JSONArray {
+							var videos = [Videos]()
+							for entry in entries {
+								let entry = Videos(data: entry as! JSONDictionary)
+								videos.append(entry)
+								
 							}
-						}
+							
+							let length = videos.count
+							print("Total:\(length)")
+							
+							let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+							dispatch_async(dispatch_get_global_queue(priority, 0)){
+								dispatch_async(dispatch_get_main_queue()){
+									completion(videos)
+								}
+							}
 					}
 				} catch {
 					dispatch_async(dispatch_get_main_queue()) {
-						completion(result: "error in JSONSerialization")
+						completion([])
 					}
 				}
 			}
--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -9,23 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		let api = APIManager()
 		api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
-		//		api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json") {
-		//			(result: String) in
-		//			print(result)
-		//		}
 	}
 	
-	func didLoadData(result: String) {
-		let alert = UIAlertController(title: result, message: nil, preferredStyle: .Alert)
-		let okAction = UIAlertAction(title: "OK", style: .Default, handler: doSomething)
-		alert.addAction(okAction)
-		self.presentViewController(alert, animated: true, completion: nil)
+	func didLoadData(videos: [Videos]) {
+		if videos.count <= 0 {
+			return
+		}
+		
+		for (index, item) in videos.enumerate(){
+			print("\(index) - name = \(item.vName)")
+		}
 	}
 	
 	func doSomething(action: UIAlertAction) {
